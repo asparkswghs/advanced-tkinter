02f4e1776cc9feb170ab618be214492d24b27858
asparkswghs <as051807@wgcloud.org>
2024-01-23 10:23:14 -0600

    triangle drawing

--- lines.py
+++ lines.py
@@ -3,22 +3,25 @@ import tkinter
 
 # Window Properties
 window = tkinter.Tk()
-window.geometry("500x400")
-window.minsize(300, 300)
+window.geometry("800x600")
+window.minsize(400, 300)
 window.title("Drawing Lines :p")
 
 # Triangle
 # Draw Right-angle Triangle by side lengths [a, b] (calculates own C)
-triangle = [10, 15]
+triangle = [500, 250]
 line_width = 2
 
-coord_origin = (10, 10)
+coord_origin = (5, 5)
 coord_a = (coord_origin[0]+triangle[0], coord_origin[1],)
 coord_b = (coord_a[0], coord_origin[1]+triangle[1])
 
 # Canvas
 canvas = tkinter.Canvas(window)
 
+canvas.create_line(*coord_origin, *coord_a, width=line_width)
+canvas.create_line(*coord_origin, *coord_b, width=line_width)
+canvas.create_line(*coord_b, *coord_a, width=line_width)
 
 canvas.pack()
 
